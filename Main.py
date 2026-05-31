d5fe14a87a47a164376f2b0b528259b5afbee11c
SurfyGalaxy <ashton3141@gmail.com>
2026-05-31 21:17:57 +1000

    Added all the logical operators

--- Main.py
+++ Main.py
@@ -19,10 +19,43 @@ while count < len(keys): #iterate over every key in instructions
     #print(keys[count]) # current keyword
     #print(values[count]) # current args
     if keys[count].startswith("print_") or keys[count] == "print": # Equal to python print() operator
-        if not func.printy(values[count][0]):
-            print(f"ValueError in {keys[count]}: No varible called '{values[count][0]}'")
+        x = func.printy(values[count][0])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
 
     elif keys[count].startswith("set_") or keys[count] == "set": # Equal to python assignment ( x = "value" )
-        if not func.set(values[count][0], values[count][1]):
-            print(f)
+        x = func.set(values[count][0], values[count][1])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+
+    elif keys[count].startswith("equality_") or keys[count] == "equality":
+        x = func.equality(values[count][0], values[count][1])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+    
+    elif keys[count].startswith("greater_") or keys[count] == "greater":
+        x = func.greater(values[count][0], values[count][1])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+
+    elif keys[count].startswith("less_") or keys[count] == "less":
+        x = func.less(values[count][0], values[count][1])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+
+    elif keys[count].startswith("or_") or keys[count] == "or":
+        x = func.ory(values[count][0], values[count][1])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+    
+    elif keys[count].startswith("and_") or keys[count] == "and":
+        x = func.andy(values[count][0], values[count][1])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+
+    elif keys[count].startswith("not_") or keys[count] == "not":
+        x = func.noty(values[count][0])
+        if x != True:
+            print(f"Error in {keys[count]}: {x}")
+    
     count += 1
--- functions.py
+++ functions.py
@@ -2,16 +2,85 @@ variables = {}
 
 def printy(args):
     global variables
-    if args.startswith('$'):
-        try:
-            args = variables[args]
-        except KeyError:
-            return False
-    print(args)
-    return True
+    try:
+        if args.startswith('$'):
+                args = variables[args]
+        print(args)
+        return True
+    except Exception as e:
+        return e
 
 def set(name, value):
     global variables
-    if not name.startswith('$'):
-        name = '$' + name
-    variables[name] = value
+    try:
+        if not name.startswith('$'):
+            name = '$' + name
+        variables[name] = value
+        return True
+    except Exception as e:
+        return e
+    
+
+def equality(a, b):
+    global variables
+    try:
+        if a.startswith('$'):
+            a = variables[a]
+        if b.startswith('$'):
+            b = variables[b]
+        return a == b
+    except Exception as e:
+        return e
+
+def greater(a, b):
+    global variables
+    try:
+        if a.startswith('$'):
+            a = variables[a]
+        if b.startswith('$'):
+            b = variables[b]
+        return a > b
+    except Exception as e:
+        return e
+
+def less(a, b):
+    global variables
+    try:
+        if a.startswith('$'):
+            a = variables[a]
+        if b.startswith('$'):
+            b = variables[b]
+        return a < b
+    except Exception as e:
+        return e
+
+def ory(a, b):
+    global variables
+    try:
+        if a.startswith('$'):
+            a = variables[a]
+        if b.startswith('$'):
+            b = variables[b]
+        return a or b
+    except Exception as e:
+        return e
+
+def andy(a, b):
+    global variables
+    try:
+        if a.startswith('$'):
+            a = variables[a]
+        if b.startswith('$'):
+            b = variables[b]
+        return a and b
+    except Exception as e:
+        return e
+
+def noty(a):
+    global variables
+    try:
+        if a.startswith('$'):
+            a = variables[a]
+        return not a
+    except Exception as e:
+        return e
